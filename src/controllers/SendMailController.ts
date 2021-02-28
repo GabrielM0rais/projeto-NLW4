a99aecae14ff4812466866361416d6f252b302bf
GabrielM0rais <revoltabrg@gmail.com>
2021-02-27 21:46:59 -0300

    quarta_aula

---
+++ src/controllers/SendMailController.ts
@@ -0,0 +1,68 @@
+import { Request, Response } from 'express';
+import { resolve } from 'path';
+import { getCustomRepository } from 'typeorm';
+import { SurveysRepository } from '../repositories/SurveysRepository';
+import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
+import { UsersRepository } from '../repositories/UsersRepository';
+import SendMailService from '../services/SendMailService';
+
+class SendMailController{ 
+    async execute(req: Request, res: Response){
+        const {email, survey_id} = req.body;
+
+        const userRepository = getCustomRepository(UsersRepository);
+        const surveyRepository = getCustomRepository(SurveysRepository)
+        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository)
+
+        
+        const user = await userRepository.findOne({ email })
+        if(!user) {
+            return res.status(404).json({
+                error: "User does not exists",
+            });
+        }
+        
+        const survey = await surveyRepository.findOne({ id: survey_id })
+        if(!survey) {
+            return res.status(404).json({
+                error: "Survey does not exists",
+            });
+        }
+        
+        const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs")
+
+        const variables = {
+            name: user.name,
+            title: survey.title,
+            description: survey.description,
+            user_id: user.id,
+            link: process.env.URL_MAIL
+        }
+
+        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({ 
+            where: [{user_id: user.id}, {value: null}],
+            relations: ["user", "survey"]
+        })
+
+        if (surveyUserAlreadyExists) {
+            await SendMailService.execute(email, survey.title, variables, npsPath);
+            return res.json(surveyUserAlreadyExists)
+        }
+
+        // Salvar informações na tabela survey-user
+        const surveyUser = surveysUsersRepository.create({
+            user_id: user.id,
+            survey_id
+        })
+        
+        await surveysUsersRepository.save(surveyUser);
+        
+        // Enviar e-mail para o usuario
+        await SendMailService.execute(email, survey.title, variables, npsPath);
+
+        return res.json(surveyUser);
+    }
+}
+
+export { SendMailController };
+
---
+++ src/services/SendMailService.ts
@@ -0,0 +1,44 @@
+import nodemailer, { Transporter } from 'nodemailer';
+import { resolve } from 'path';
+import fs from 'fs';
+import handlebars from 'handlebars';
+
+class SendMailService {
+    private client: Transporter;
+
+    constructor() {
+        nodemailer.createTestAccount().then(account => {
+           const transporter = nodemailer.createTransport({
+               host: account.smtp.host,
+                port: account.smtp.port,
+                secure: account.smtp.secure,
+                auth: {
+                    user: account.user,
+                    pass: account.pass,
+                }
+           })
+
+           this.client = transporter;
+        });
+    }
+
+    async execute(to: string, subject: string, variables: object, path: string){
+        const templateFileCOntent = fs.readFileSync(path).toString("utf8")
+
+        const mailTemplateParse = handlebars.compile(templateFileCOntent)
+
+        const html = mailTemplateParse(variables)
+
+        const messege = await this.client.sendMail({
+            to, 
+            subject, 
+            html,
+            from: "NPS <noreplay@nps.com.br>"
+        });
+
+        console.log("Messge send: %s", messege.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(messege));
+    }
+}
+
+export default new SendMailService();
